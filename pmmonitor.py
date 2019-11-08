8165567f35994bba8f49a6da7313c9f3389238a2
SophonAlpha <SophonAlpha@users.noreply.github.com>
2019-11-08 21:43:23 +0400

    change to proper hex notation for all frame bytes

--- pmmonitor.py
+++ pmmonitor.py
@@ -18,7 +18,7 @@ def set_up_logging():
 
 class SHDLC:
     def __init__(self):
-        self.valid_states = ['00', '01', '02', '03', '04', '28', '43']
+        self.valid_states = ['0x0', '0x1', '0x2', '0x3', '0x4', '0x28', '0x43']
         self.last_cmd = None
         self.buffer_size = 64
         self.port = self.open_serial_port()
@@ -41,18 +41,18 @@ class SHDLC:
     def send_command(self, cmd, data):
         mosi_frame = build_mosi_frame(cmd, data)
         my_logger.info('sending MOSI frame: {}'.format(mosi_frame))
+        mosi_frame = ''.join(['{:02x}'.format(int(byte, 0)) for byte in mosi_frame])
         mosi_frame = bytes.fromhex(mosi_frame)
         self.port.write(mosi_frame)
         self.last_cmd = cmd
 
     def get_response(self):
         data = self.port.read(self.buffer_size)
-        miso_frame = ['{}'.format(hex(data[pos])) for pos in range(len(data))]
-        my_logger.info('received MISO frame: {}'.format(' '.join(miso_frame)))
-        valid, err_msg = self.validate_miso_frame(miso_frame)
-        if valid:
-            # TODO: extract data from MISO frame
-            pass
+        miso_frame = [hex(data[pos]) for pos in range(len(data))]
+        my_logger.info('received MISO frame: {}'.format(miso_frame))
+        self.validate_miso_frame(miso_frame)
+        self.check_state(miso_frame)
+        # TODO: extract data from MISO frame
         return data
     
     def validate_miso_frame(self, miso_frame):
@@ -61,12 +61,12 @@ class SHDLC:
         miso_frame = byte_unstuffing(miso_frame)
         miso_frame_int = [int(byte, 16) for byte in miso_frame]
         chk = calculate_checksum(miso_frame_int[1:-2])
-        if not miso_frame[0] == '7E':  # start
+        if not miso_frame[0] == '0x7e':  # start
             valid = False
             err_msg = 'MISO frame start byte {} invalid. Expected: \'{}\'. ' \
                       'Received: \'{}\''.format(0, '7E', miso_frame[0])
             my_logger.error(err_msg)
-        elif not miso_frame[1] == '00':  # address
+        elif not miso_frame[1] == '0x0':  # address
             valid = False
             err_msg = 'MISO frame address byte {} invalid. Expected: \'{}\'. ' \
                       'Received: \'{}\''.format(1, '00', miso_frame[1])
@@ -94,9 +94,9 @@ class SHDLC:
                       'Received: \'{}\''.format(len(miso_frame) - 2,
                                                 format(chk, 'x').upper(),
                                                 format(miso_frame_int[-2], 'x').upper())
-        elif not miso_frame[-1] == '7E':  # end
+        elif not miso_frame[-1] == '0x7e':  # end
             valid = False
-            err_msg = 'MISO frame end byte {} invalid. Expected: \'{}\'. ' \
+            err_msg = 'MISO frame stop byte {} invalid. Expected: \'{}\'. ' \
                       'Received: \'{}\''.format(len(miso_frame) - 1, '7E', miso_frame[-1])
             my_logger.error(err_msg)
         return valid, err_msg
@@ -104,14 +104,14 @@ class SHDLC:
 
 class Commands:
     def __init__(self):
-        self.start_measurement = '00'
-        self.stop_measurement = '01'
-        self.read_measured_values = '03'
-        self.read_auto_cleaning_interval = '80'
-        self.write_auto_cleaning_interval = '80'
-        self.start_fan_cleaning = '56'
-        self.device_information = 'D0'
-        self.device_reset = 'D3'
+        self.start_measurement = '0x0'
+        self.stop_measurement = '0x1'
+        self.read_measured_values = '0x3'
+        self.read_auto_cleaning_interval = '0x80'
+        self.write_auto_cleaning_interval = '0x80'
+        self.start_fan_cleaning = '0x56'
+        self.device_information = '0xd0'
+        self.device_reset = '0xd3'
 
 
 class SensirionSPS30:
@@ -120,8 +120,16 @@ class SensirionSPS30:
         self.cmd = Commands()
 
     def start_measurement(self):
-        data = '01 03'  # as per Sensirion SPS30 datasheet
-        self.shdlc.send_command(self.cmd.start_measurement, data)
+        data = ['0x1', '0x3']  # as per Sensirion SPS30 datasheet
+        try:
+            self.shdlc.send_command(self.cmd.start_measurement, data)
+        except SendCommandError as err:
+            # TODO: add error handling code
+            pass
+        except SerialError as err
+            # TODO: add error handling code
+            pass
+        
         rsp = self.shdlc.get_response()
 
     def stop_measurement(self):
@@ -160,21 +168,19 @@ def build_mosi_frame(command, data=''):
     """
 
     frame = []
-    frame.append('7E')  # start
-    frame.append('00')  # address
+    frame.append('0x7e')  # start
+    frame.append('0x0')  # address
     frame.append(command)  # command
-    frame.append('{:02x}'.format(len(data.split())))  # length
-    frame.append(''.join(data.split())) # TX data
-    # TODO: simplify the checksum calculation
+    frame.append('0x{:x}'.format(len(data)))  # length
+    frame.extend(data) # TX data
     check = calculate_checksum([int(frame[1], 16), int(frame[2], 16),
                                 int(frame[3], 16)] + [int(byte, 16)
-                                                      for byte in data.split()])
-    check = '{:02x}'.format(check)
+                                                      for byte in data])
+    check = '0x{:x}'.format(check)
     frame.append(check)  # check
-    frame.append('7E')  # stop
+    frame.append('0x7e')  # stop
     frame = [byte.upper() for byte in frame]
     frame = byte_stuffing(frame)
-    frame = ''.join(frame)
     return frame
 
 
@@ -199,7 +205,7 @@ def byte_stuffing(frame):
     """
     Do byte-stuffing.
 
-    The 0x7E character is sent at the beginning and at the end of the frame to
+    The 0x7e character is sent at the beginning and at the end of the frame to
     signalize frame start and stop. If this byte (0x7E) occurs anywhere else in
     the frame, it must be replaced by two other bytes (byte-stuffing).
     This also applies to the characters 0x7D, 0x11 and 0x13.
@@ -208,10 +214,10 @@ def byte_stuffing(frame):
     :return: new 'byte stuffed' frame
     """
     stuffing = {
-        '7E': ['7D', '5E'],
-        '7D': ['7D', '5D'],
-        '11': ['7D', '31'],
-        '13': ['7D', '33'],
+        '0x7e': ['0x7d', '0x5e'],
+        '0x7d': ['0x7d', '0x5d'],
+        '0x11': ['0x7d', '0x31'],
+        '0x13': ['0x7d', '0x33'],
     }
     new_frame = []
     new_frame.append(frame[0])  # add start frame
@@ -237,10 +243,10 @@ def byte_unstuffing(frame):
     :return: new 'byte stuffed' frame
     """
     unstuffing = {
-        ('7D', '5E'): '7E',
-        ('7D', '5D'): '7D',
-        ('7D', '31'): '11',
-        ('7D', '33'): '13',
+        ('0x7d', '0x5e'): '0x7e',
+        ('0x7d', '0x5d'): '0x7d',
+        ('0x7d', '0x31'): '0x11',
+        ('0x7d', '0x33'): '0x13',
     }
     ptr = 0
     key_len = len(list(unstuffing.keys())[0])
@@ -258,9 +264,9 @@ my_logger = set_up_logging()
 if __name__ == '__main__':
     # start measurement
     shdlc = SHDLC()
-    resp_frame = ['7E', '00', '80', '01', '00', '7D', '5E', '7E']
-    shdlc.last_cmd = '80'
-    print(shdlc.validate_miso_frame(resp_frame))
-    # pm_sensor = SensirionSPS30()
-    # resp = pm_sensor.start_measurement()
-    # print(resp)
+#    resp_frame = ['7E', '00', '80', '01', '00', '7D', '5E', '7E']
+#    shdlc.last_cmd = '80'
+#    print(shdlc.validate_miso_frame(resp_frame))
+    pm_sensor = SensirionSPS30()
+    resp = pm_sensor.start_measurement()
+    print(resp)
